cca33ef32537a79d01364cdc2b8424126c7ba012
Madhu <madhuanbarasu19@gmail.com>
2025-12-25 02:33:34 +0530

    Configure Kafka JSON serialization for Transaction

--- src/main/java/com/jpmc/midascore/config/KafkaConfig.java
+++ src/main/java/com/jpmc/midascore/config/KafkaConfig.java
@@ -1,4 +1,22 @@
 package com.jpmc.midascore.config;
 
+import com.jpmc.midascore.foundation.Transaction;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+
+@Configuration
 public class KafkaConfig {
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, Transaction>
+    kafkaListenerContainerFactory(ConsumerFactory<String, Transaction> consumerFactory) {
+
+        ConcurrentKafkaListenerContainerFactory<String, Transaction> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory);
+        return factory;
+    }
 }
+
